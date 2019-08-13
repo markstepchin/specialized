5e5351367d96ba3dc2fb9ef85da28dc6ddd30e92
markstepchin <markstepchin@gmail.com>
2019-08-13 16:52:51 -0700

    Add the top header

---
+++ src/Layout/HeaderTop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <span>Free shipping on orders over $150.</span>
+    <div>
+      <span><span className='find-retailer-icon'/>Find a retailer</span>
+      <span><span className='flag-icon'/>EN</span>
+      <button>Sign In</button>
+    </div>
+  </header>
+);
+
+export default Header;
