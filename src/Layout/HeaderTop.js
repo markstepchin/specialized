1664d66ebff34b1669ace2b180d996e991783917
markstepchin <markstepchin@gmail.com>
2019-08-13 18:11:21 -0700

    Add the lower header

--- src/Layout/HeaderTop.js
+++ src/Layout/HeaderTop.js
@@ -2,11 +2,26 @@ import React from 'react';
 
 const Header = () => (
   <header>
-    <span>Free shipping on orders over $150.</span>
-    <div>
-      <span><span className='find-retailer-icon'/>Find a retailer</span>
-      <span><span className='flag-icon'/>EN</span>
-      <button>Sign In</button>
+    <div className='top-header'>
+      <span>Free shipping on orders over $150.</span>
+      <div>
+        <span><span className='find-retailer-icon'/>Find a retailer</span>
+        <span><span className='flag-icon'/>EN</span>
+        <button>Sign In</button>
+      </div>
+    </div>
+    <div className='bottom-header'>
+      <a className='logo' href='#' />
+      <ul>
+        <li><a href='#'>Bikes</a></li>
+        <li><a href='#'>Downhill</a></li>
+        <li><a href='#'>S-works</a></li>
+        <li><a href='#'>inside specialized</a></li>
+      </ul>
+      <div>
+        <span className='search-icon'/>
+        <span className='cart-icon'/>
+      </div>
     </div>
   </header>
 );
