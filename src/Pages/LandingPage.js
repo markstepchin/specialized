31908be353f60f9f2796ce67d88286aab733d728
markstepchin <markstepchin@gmail.com>
2019-08-14 11:47:48 -0700

    Add hero section to landing page

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const LandingPage = () => (
-  <h1>landing page</h1>
+  <>
+    <section id='hero'>
+      <div id='hero-text'>
+        <h1>We have <br />the works</h1>
+        <h3>Its all fun and games until you catch too much air</h3>
+        <button>demo 8 fsr</button>
+      </div>
+    </section>
+    <section id='explore'>explore</section>
+    <section id='image'>image</section>
+    <section id='video'>video</section>
+    <section id='instagram'>instagram</section>
+  </>
 );
 
 export default LandingPage;
