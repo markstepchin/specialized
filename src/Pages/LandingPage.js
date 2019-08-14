f6e6004a0e4d941dd20308a8f01bddc3b5c64dee
markstepchin <markstepchin@gmail.com>
2019-08-14 11:48:25 -0700

    Add the story section to the landing page

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -9,6 +9,11 @@ const LandingPage = () => (
         <button>demo 8 fsr</button>
       </div>
     </section>
+    <section id='story'>
+      <h2>SPECIALIZED MTB</h2>
+      <p>We are riders—that fact has guided our every decision since 1974. When quality tires weren't around, we strove to make the best. When people wanted to ride cruisers in the dirt, we made the first production mountain bike. When roadies wanted to go faster, we doubled-down on carbon and built our own wind tunnel. And when we saw kids struggling to focus in school, we started the Specialized Foundation to help them through cycling.</p>
+      <button>our story</button>
+    </section>
     <section id='explore'>explore</section>
     <section id='image'>image</section>
     <section id='video'>video</section>
