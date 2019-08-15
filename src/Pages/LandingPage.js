e12d28ad3b18400f64922c545636fc765e1fd365
markstepchin <markstepchin@gmail.com>
2019-08-15 16:56:44 -0700

    Make landing page responsize for any screen size

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -7,7 +7,7 @@ const LandingPage = () => (
     <div id='hero'>
       <div id='hero-text'>
         <h1>We have <br />the works</h1>
-        <h3>Its all fun and games until you catch too much air</h3>
+        <h3>Its all fun and games until you catch too much air...</h3>
         <button>demo 8 fsr</button>
       </div>
     </div>
