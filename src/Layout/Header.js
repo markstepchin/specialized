37f866eb002eaab2cd117024662e1d70e1afc838
markstepchin <markstepchin@gmail.com>
2019-08-15 13:30:19 -0700

    Toggle toolbar from header

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,19 +1,56 @@
 import React from 'react';
 
-const Header = () => (
-  <header>
-    {/* <ToolBar /> */}
-    <div className='top-header'>
-      <span>Free shipping on orders over $150.</span>
-      <div>
-        <span><span className='find-retailer-icon'/>Find a retailer</span>
-        <span><span className='flag-icon'/>EN</span>
-        <button>Sign In</button>
+class Header extends React.Component {
+  state = {
+    toggleOpen: false
+  }
+
+  onToggleClick = () => this.setState({ toggleOpen: !this.state.toggleOpen });
+
+  render() {
+    return (
+      <header>
+        <ToolBar isOpen={this.state.toggleOpen} onClick={this.onToggleClick}/>
+        <div className='top-header'>
+          <span>Free shipping on orders over $150.</span>
+          <div>
+            <span><span className='find-retailer-icon'/>Find a retailer</span>
+            <span><span className='flag-icon'/>EN</span>
+            <button>Sign In</button>
+          </div>
+        </div>
+        <div className='bottom-header'>
+          <button id='menu-button' onClick={this.onToggleClick}>
+            <ion-icon name="menu"></ion-icon>
+          </button>
+          <a className='logo' href='/#' />
+          <ul>
+            {/* <li><a href='/#'>Bikes</a></li> */}
+            <li><a href='/#'>Trail</a></li>
+            <li><a href='/#'>Downhill</a></li>
+            <li><a href='/#'>S-works</a></li>
+            <li><a href='/#'>inside specialized</a></li>
+          </ul>
+          <div>
+            <span className='search-icon'/>
+            <span className='cart-icon'/>
+          </div>
+        </div>
+      </header>
+    );
+  }
+};
+
+const ToolBar = ({ isOpen, onClick }) => (
+  isOpen && 
+    <div id='toolbar'>
+      <div id='toolbar-header'>
+        <button id='close-toobar-button' onClick={onClick}>
+          <ion-icon name="close"></ion-icon>
+        </button>
+        <div>Shop</div>
+        <a href='/#'>Inside specialized</a>
       </div>
-    </div>
-    <div className='bottom-header'>
-      <button id='menu-button'><ion-icon name="menu"></ion-icon></button>
-      <a className='logo' href='/#' />
       <ul>
         {/* <li><a href='/#'>Bikes</a></li> */}
         <li><a href='/#'>Trail</a></li>
@@ -21,29 +58,7 @@ const Header = () => (
         <li><a href='/#'>S-works</a></li>
         <li><a href='/#'>inside specialized</a></li>
       </ul>
-      <div>
-        <span className='search-icon'/>
-        <span className='cart-icon'/>
-      </div>
     </div>
-  </header>
-);
-
-const ToolBar = () => (
-  <div id='toolbar'>
-    <div id='toolbar-header'>
-      <button id='close-toobar-button'><ion-icon name="close"></ion-icon></button>
-      <div>Shop</div>
-      <a href='/#'>Inside specialized</a>
-    </div>
-    <ul>
-      <li><a href='/#'>Bikes</a></li>
-      <li><a href='/#'>Trail</a></li>
-      <li><a href='/#'>Downhill</a></li>
-      <li><a href='/#'>S-works</a></li>
-      <li><a href='/#'>inside specialized</a></li>
-    </ul>
-  </div>
 );
 
 export default Header;
