8e147ac74edd8e9f72b38f0d874006ed26bd781c
markstepchin <markstepchin@gmail.com>
2019-08-15 17:42:49 -0700

    Place landing page into a container that doesn't expand past 1200px

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -3,7 +3,7 @@ import instaLogo from "../Assets/Images/Landing/IGLogo.png";
 import verifiedLink from "../Assets/Icons&Logos/IG/Verified.svg";
 
 const LandingPage = () => (
-  <>
+  <div id='page-container'>
     <div id='hero'>
       <div id='hero-text'>
         <h1>We have <br />the works</h1>
@@ -58,7 +58,7 @@ const LandingPage = () => (
         <li><div id='img6'/></li>
       </ul>
     </section>
-  </>
+  </div>
 );
 
 export default LandingPage;
