e2c15b73ca73a4d276085853c19af6b1ec05d6cb
markstepchin <markstepchin@gmail.com>
2019-08-17 09:10:25 -0700

    Refactor listing component to accept dynamic data

--- src/Pages/BikeListing.js
+++ src/Pages/BikeListing.js
@@ -1,17 +1,15 @@
 import React from "react";
-import bikeHeroImage from "../Assets/Images/Bikes/BikesHero.jpg";
-import bikeData from "./BikeData";
 
-const BikeListing = () => (
+const BikeListing = ({ data, title, description, children }) => (
   <div id='listing-container'>
     <div id='hero-img'>
-      <img src={bikeHeroImage} alt='bike cover'/>
-      <span id='hero-text'>Bikes</span>
+      {children}
+      <span id='hero-text'>{title}</span>
     </div>
     <div id='description'>
-      <p>Perfection. It's hard to define, yet easy to recognize. We specialize in the science of perfection, however, and every road bike we make embodies it. No matter your motivation or discipline, you'll benefit from our attention to detail and innovative spirit. So whether you're getting dirty on cyclocross bikes, leaving it all on the road, or testing your limits in a triathlon, you can rest assured that the bike underneath you is simply the best.</p>
+      {description && <p>{description}</p>}
     </div>
-    {bikeData.map(bd => 
+    {data.map(bd => 
       <ListSection 
         key={bd.id}
         title={bd.title}
