cdb92adcd363500e56b1612e051dd372a2ba6453
markstepchin <markstepchin@gmail.com>
2019-08-21 14:34:58 -0700

    Add the gallery component

---
+++ src/Layout/Gallery.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image, Dot } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
+export default ({ images }) => (
+  <div>
+    <CarouselProvider
+      naturalSlideWidth={100}
+      naturalSlideHeight={35}
+      totalSlides={images.length}
+      orientation='horizontal'
+    >
+      <Slider className='slider'>
+        {images.map((src, i) => (
+          <Slide index={i}>
+            <Image src={src}/>
+          </Slide>
+        ))}
+      </Slider>
+      <div id='gallery-button-container'>
+        <ButtonBack className='gallery-buttons'>
+          <ion-icon name="arrow-back"></ion-icon>
+        </ButtonBack>
+        <ButtonNext className='gallery-buttons'>
+          <ion-icon name="arrow-forward"></ion-icon>
+        </ButtonNext>
+      </div>
+      <div className='dot-group'>
+        <div>
+          {images.map((src, i) => <Dot slide={i} className='gallery-dot'/>)}
+        </div>  
+      </div>
+    </CarouselProvider>
+  </div>
+);
--- src/Layout/ProductPage.js
+++ src/Layout/ProductPage.js
@@ -1,17 +1,21 @@
 import React from "react";
+import Gallery from "./Gallery";
+
+const images = [
+  "https://s7d5.scene7.com/is/image/Specialized/?layer=0&wid=1920&hei=640&fmt=jpg&src=is{Specialized/pdp-product-bg-dark?wid=1920&hei=640}&layer=1&src=is{Specialized/97518-32_SJ-FSR-MEN-EXPERT-CARBON-29-CKD-STRMGRY-RKTRED_HERO?wid=920&hei=600&$hybris-pdp-hero$}",
+  "https://s7d5.scene7.com/is/image/Specialized/?layer=0&wid=1920&hei=640&fmt=jpg&src=is{Specialized/pdp-product-bg-dark-fb?wid=1920&hei=640}&layer=1&src=is{Specialized/97518-32_SJ-FSR-MEN_EXPERT-CARBON-29_STRMGRY-RKTRED_DETAIL-1?wid=1920&hei=640&$hybris-pdp-hero-exp$}",
+  "https://s7d5.scene7.com/is/image/Specialized/?layer=0&wid=1920&hei=640&fmt=jpg&src=is{Specialized/pdp-product-bg-dark-fb?wid=1920&hei=640}&layer=1&src=is{Specialized/97518-32_SJ-FSR-MEN_EXPERT-CARBON-29_STRMGRY-RKTRED_DETAIL-2?wid=1920&hei=640&$hybris-pdp-hero-exp$}",
+  "https://s7d5.scene7.com/is/image/Specialized/?layer=0&wid=1920&hei=640&fmt=jpg&src=is{Specialized/pdp-product-bg-dark-fb?wid=1920&hei=640}&layer=1&src=is{Specialized/97518-32_SJ-FSR-MEN_EXPERT-CARBON-29_STRMGRY-RKTRED_DETAIL-3?wid=1920&hei=640&$hybris-pdp-hero-exp$}"
+];
 
 const ProductPage = () => (
-  <>
-    <ImageGallery />
+  <div style={{ paddingTop: '10rem' }}>
+    <Gallery images={images}/>
     <DisplayFooter />
     <Description />
     <TechnicalSpecs />
     <Reviews />
-  </>
-);
-
-const ImageGallery = () => (
-  <h1>image gallery</h1>
+  </div>
 );
 
 const DisplayFooter = () => (
