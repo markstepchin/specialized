e2768b29a7ff3385e6d4d95c6bb55b319f6cc28c
markstepchin <markstepchin@gmail.com>
2019-08-21 15:56:01 -0700

    Add gallery footer details

--- src/Layout/ProductPage.js
+++ src/Layout/ProductPage.js
@@ -8,30 +8,44 @@ const images = [
   "https://s7d5.scene7.com/is/image/Specialized/?layer=0&wid=1920&hei=640&fmt=jpg&src=is{Specialized/pdp-product-bg-dark-fb?wid=1920&hei=640}&layer=1&src=is{Specialized/97518-32_SJ-FSR-MEN_EXPERT-CARBON-29_STRMGRY-RKTRED_DETAIL-3?wid=1920&hei=640&$hybris-pdp-hero-exp$}"
 ];
 
+const details = {
+  title: 'Stumpjumper EVO Comp Carbon 27.5',
+  price: '4,520.00',
+  partNumber: '97519-5402',
+}
+
 const ProductPage = () => (
-  <div style={{ paddingTop: '10rem' }}>
+  <div style={{ paddingTop: '10rem', margin: '0 2rem' }}>
     <Gallery images={images}/>
-    <DisplayFooter />
+    <DisplayFooter details={details}/>
     <Description />
     <TechnicalSpecs />
     <Reviews />
   </div>
 );
 
-const DisplayFooter = () => (
-  <div>
-    <div>
-      <h2>Stumpjumper EVO Comp Carbon 27.5</h2>
-      <h3>$4,520.00</h3>
-      <p>Part No.  97519-5402</p>
+const DisplayFooter = ({ details: { title, price, partNumber } }) => (
+  <section className='display-footer-container'>
+    <div className='section-1'>
+      <h2 className='display-title'>
+        {title}
+      </h2>
+      <h3 className='display-price'>
+        ${price}
+      </h3>
+      <p className='display-partNumber'>
+        Part No.{' '}{partNumber}
+      </p>
     </div>
-    <span>In Stock</span>
-    <div>
-      <button>ship to store</button>
+
+    <span className='in-stock'>In Stock</span>
+
+    <div className='section-3'>
+      <button>add to cart</button>
       <button>tune my suspension</button>
       <button>find nearby</button>
     </div>
-  </div>
+  </section>
 );
 
 const Description = () => (
