6dbac2d6f8433b1fd3da6c403069671cb714cc25
markstepchin <markstepchin@gmail.com>
2019-08-22 18:44:36 -0700

    Style empty shopping chart

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Cart = () => (
   <section id='cart'>
+    <h1>Shopping cart</h1>
     <EmptyCartMessage />
     <CartFooter />
   </section>
@@ -16,34 +17,28 @@ const CartContents = () => (
 );
 
 const EmptyCartMessage = () => (
-  <div>
-    <div>
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div id='empty-cart'>
       <h2>Your Shopping Cart is Empty</h2>
-      <span>Nothin' to see here.</span>
+      <p>Nothin' to see here.</p>
       <Link to='/shop-bikes'>Continue Shopping</Link>
     </div>
   </div>
 )
 
 const CartFooter = () => (
-  <div>
-    <div>
+  <div id='cart-footer'>
+    <div className='cart-footer-col'>
       <h3>Contact Us</h3>
       <p>8:00am - 6:00pm MST, Monday-Friday</p>
       <p>877-808-8154</p>
       <p>ridercare@specialized.com</p>
     </div>
-    <div>
+    <div className='cart-footer-col'>
       <h3>Helpful Links</h3>
-      <a>Frequently Asked Questions</a>
-      <a>Return Policy</a>
-      <a>Warranty Policy</a>
-    </div>
-    <div>
-      <h3>We Accept</h3>
-      <div>
-        card icons
-      </div>
+      <a href='https://www.specialized.com/us/en/faq'>Frequently Asked Questions</a>
+      <a href='https://www.specialized.com/us/en/faq#returns'>Return Policy</a>
+      <a href='https://www.specialized.com/us/en/warranty'>Warranty Policy</a>
     </div>
   </div>
 )
