3c1d6c950d64541cb76efe8dcc10f041b985aa54
markstepchin <markstepchin@gmail.com>
2019-08-23 22:39:55 -0700

    Show the cart preview for 5 seconds after clicking add to cart button

--- src/CartContext.js
+++ src/CartContext.js
@@ -1,15 +1,26 @@
 import React from "react";
 
+const SEC_SHOW_CART_PREVIEW = 5;
+
 class CartContainer extends React.Component {
   state = {
     items: [
       'hello world',
       'whats up',
       'heeeeeeey'
-    ]
+    ],
+    itemAdded: false
   }
 
-  addItem = id => this.setState({ items: [...this.state.items, id] });
+  addItem = id => {
+    this.setState({ 
+      items: [...this.state.items, id],
+      itemAdded: true
+    });
+    
+    setTimeout(() => this.setState({itemAdded: false}), SEC_SHOW_CART_PREVIEW * 1000);
+  }
+
   removeItem = id => {
     const newArray = [...this.state.items];
     newArray.splice(newArray.indexOf(id), 1);
@@ -22,7 +33,8 @@ class CartContainer extends React.Component {
       <CartContext.Provider value={{ 
           items: this.state.items,
           addItem: this.addItem,
-          removeItem: this.removeItem
+          removeItem: this.removeItem,
+          itemAdded: this.state.itemAdded
         }}>
         {this.props.children}
       </CartContext.Provider>
--- src/Layout/CartPreview.js
+++ src/Layout/CartPreview.js
@@ -1,42 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from 'react-router-dom';
+import { CartContext } from '../CartContext';
 
-export default () => {
-  const [hover, setHover] = useState(true);
+class CartPreview extends React.Component {
+  state = {
+    hover: false
+  }
 
-  return (
-    <>
-      <Link 
-        to='/cart' 
-        className='cart-icon'
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      >
-        <ion-icon name="cart"></ion-icon>
-      </Link>
-      {hover && (
-        <div id='cart-preview'>
-          <div id='preview-item-listing'>
-            <div style={{ display: 'flex' }}>
-              <img  alt='' style={{ height: '150px', width: '150px' }}/>
-              <div className='details'>
-                <h3>Stumpjumper EVO</h3>
-                <h4>$4,520.00</h4>
-                <h4>QTY 1</h4>
+  render() {
+    return (
+      <>
+        <Link 
+          to='/cart' 
+          className='cart-icon'
+          onMouseEnter={() => this.setState({ hover: true })}
+          onMouseLeave={() => this.setState({ hover: false })}
+        >
+          <ion-icon name="cart"></ion-icon>
+        </Link>
+        {(this.state.hover || this.context.itemAdded) && (
+          <div id='cart-preview'>
+            <div id='preview-item-listing'>
+              <div style={{ display: 'flex' }}>
+                <img  alt='' style={{ height: '150px', width: '150px' }}/>
+                <div className='details'>
+                  <h3>Stumpjumper EVO</h3>
+                  <h4>$4,520.00</h4>
+                  <h4>QTY 1</h4>
+                </div>
+                <button className='close-button'><ion-icon name="close"></ion-icon></button>
               </div>
-              <button className='close-button'><ion-icon name="close"></ion-icon></button>
             </div>
-          </div>
-          <div>
-            <div className='subtotals'>
-              <h3>Subtotal</h3>
-              <h3>$9,040.00</h3>
+            <div>
+              <div className='subtotals'>
+                <h3>Subtotal</h3>
+                <h3>$9,040.00</h3>
+              </div>
+              <span className='taxes'>Taxes are calculated at checkout</span>
             </div>
-            <span className='taxes'>Taxes are calculated at checkout</span>
+            <Link to='/cart' className='checkout-button'>view cart and checkout</Link>
           </div>
-          <Link to='/cart' className='checkout-button'>view cart and checkout</Link>
-        </div>
-      )}
-    </>
-  );
+        )}
+      </>
+    )
+  }
 }
+
+CartPreview.contextType = CartContext;
+export default CartPreview;
