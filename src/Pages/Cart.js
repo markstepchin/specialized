1337294e5bceaa63c27c841a41b1ed311e693a8d
markstepchin <markstepchin@gmail.com>
2019-08-28 16:19:10 -0700

    Remove unused data

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { CartContext, useCartContext } from "../CartContext";
+import { useCartContext } from "../CartContext";
 import { getBikeList } from "../utils/general";
 
 const Cart = () => {
